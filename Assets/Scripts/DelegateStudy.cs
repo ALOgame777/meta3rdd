2a275fbb50505ab5f0a8756b4fb9316740d8e455
gl7273 <gl7273@naver.com>
2024-09-11 11:25:18 +0900

    0911 오전 수업 voice 추가

--- Assets/Scripts/DelegateStudy.cs
+++ Assets/Scripts/DelegateStudy.cs
@@ -36,7 +36,6 @@ public class DelegateStudy : MonoBehaviour
         action2("dddd");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/PlayerVoice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Voice.PUN;
+public class PlayerVoice : MonoBehaviour
+{
+    // Photon Voice View
+    public PhotonVoiceView pvv;
+    // image Speaker
+    public GameObject imgSpeaker;
+    // Image Speech Bubble
+    public GameObject imgSpeechBubble;
+
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        // 만약에 내가 말하고 있다면
+        // imgSpeechBubble 활성화
+        // 그렇지 않으면 비활성화
+        imgSpeechBubble.SetActive(pvv.IsRecording);
+
+        // 만약에 말하는 소리가 들린다면
+        //imgSpeaker 활성화
+        // 그렇지 않으면 비활성화
+        imgSpeechBubble.SetActive(pvv.IsSpeaking);
+    }
+
+}
